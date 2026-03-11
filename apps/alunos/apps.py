fb02c726fcbd89aa34f4765aefdbe082531f9f6a
yoshimidevz <yoshimidev1@gmail.com>
2026-03-11 20:32:25 -0300

    Related #02: Configurando o apps.py

--- apps/alunos/apps.py
+++ apps/alunos/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
 class AlunosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'alunos'
+    name = 'apps.alunos'
+    verbose_name = 'Alunos'
