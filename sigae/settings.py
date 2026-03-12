889332f4638a92da2e54c99e99a1fefa05454b87
yoshimidevz <yoshimidev1@gmail.com>
2026-03-11 21:05:53 -0300

    Related #02: Movendo apps.alunos para Installed_apps e adicionando auth user model

--- sigae/settings.py
+++ sigae/settings.py
@@ -37,13 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
-
-LOCAL_APPS = [
     'apps.alunos',
-    # 'apps.perfil_ivs',
-    # 'apps.editais',
-    # 'apps.inscricoes',
 ]
 
 MIDDLEWARE = [
@@ -89,6 +83,8 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
+
+AUTH_USER_MODEL = 'alunos.Aluno'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
