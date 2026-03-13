e5c9a006e00a626c0989b556046501f6ab3c28c1
yoshimidevz <yoshimidev1@gmail.com>
2026-03-13 18:19:55 -0300

    Related #01: Arrumando o bloco de databases no settings para não ter erro no banco na hora de subir migration

--- sigae/settings.py
+++ sigae/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -74,10 +78,7 @@ WSGI_APPLICATION = 'sigae.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL', default='sqlite:///db.sqlite3')
 }
 
 
