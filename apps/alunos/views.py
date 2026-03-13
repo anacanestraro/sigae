be2184f54ce4f71839f480a1c4ce3c08b02f8402
anacanestraro <ana.canestraro07@gmail.com>
2026-03-13 18:56:51 -0300

    Related #02: Implementação de perfil_view e editar_view de aluno

--- apps/alunos/views.py
+++ apps/alunos/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import AlunoForm
 
-# Create your views here.
+@login_required
+def perfil_view(request):
+    return render(request, 'alunos/perfil.html', {
+        'aluno': request.user
+    })
+
+@login_required
+def editar_view(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Perfil atualizado com sucesso!')
+            return redirect('alunos:perfil')
+    else:
+        form = AlunoForm(instance=request.user)
+
+    return render(request, 'alunos/editar.html', {'form': form})
